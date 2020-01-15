34b910bdcb875f4c196b0a0469b4d33f08ecf804
Lukasz Myslinski <lukasz.myslinski@friday.de>
2020-01-15 15:39:01 +0100

    Reduce max file chunk size to 700kB; Add tests

---
+++ src/test/java/org/kopi/ebics/io/SplitterTest.java
@@ -0,0 +1,115 @@
+package org.kopi.ebics.io;
+
+import org.junit.jupiter.api.Test;
+import org.kopi.ebics.client.EbicsClient;
+import org.kopi.ebics.exception.EbicsException;
+import org.kopi.ebics.session.EbicsSession;
+import org.kopi.ebics.session.OrderType;
+import org.kopi.ebics.xml.UploadTransferRequestElement;
+import org.mockito.Mockito;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Random;
+
+class SplitterTest {
+
+    private static final int ONE_KB = 1000;
+    private static final int ONE_MB = ONE_KB * ONE_KB;
+    private static final int CHUNK_SIZE_LIMIT = 700*ONE_KB;
+    private static final int REQUEST_SIZE_LIMIT = 1000*ONE_KB;
+
+    @Test
+    void testSplitOneMb() throws NoSuchAlgorithmException, InvalidKeySpecException, EbicsException, IOException {
+        byte[] b = randomDataArray(ONE_MB);
+        Splitter splitter = splitAndVerifyContent(b);
+        verifyActualRequestSize(splitter);
+    }
+
+    @Test
+    void testSplitTwoMb() throws NoSuchAlgorithmException, InvalidKeySpecException, EbicsException, IOException {
+        byte[] b = randomDataArray(2*ONE_MB);
+        Splitter splitter = splitAndVerifyContent(b);
+        verifyActualRequestSize(splitter);
+    }
+
+    @Test
+    void testSplitTenMb() throws NoSuchAlgorithmException, InvalidKeySpecException, EbicsException, IOException {
+        byte[] b = randomDataArray(10*ONE_MB);
+        Splitter splitter = splitAndVerifyContent(b);
+        verifyActualRequestSize(splitter);
+    }
+
+    @Test
+    void testSplitFiftyMb() throws NoSuchAlgorithmException, InvalidKeySpecException, EbicsException, IOException {
+        byte[] b = randomDataArray(50*ONE_MB);
+        Splitter splitter = splitAndVerifyContent(b);
+        verifyActualRequestSize(splitter);
+    }
+
+    private UploadTransferRequestElement prepareActualRequest(Splitter splitter) throws EbicsException {
+        // Not used but apparently needed. I don't even wanna know
+        Mockito.mock(EbicsClient.class, Mockito.RETURNS_DEEP_STUBS);
+        EbicsSession ebicsSession = Mockito.mock(EbicsSession.class, Mockito.RETURNS_DEEP_STUBS);
+
+        UploadTransferRequestElement uploader = new UploadTransferRequestElement(ebicsSession,
+                OrderType.CDD,
+                1,
+                false,
+                "asda".getBytes(),
+                splitter.getContent(1)
+        );
+        uploader.build();
+        return uploader;
+    }
+
+    private void verifyActualRequestSize(Splitter splitter) throws EbicsException {
+        UploadTransferRequestElement fullRequest = prepareActualRequest(splitter);
+        int actualSize = fullRequest.prettyPrint().length;
+        assert (actualSize < REQUEST_SIZE_LIMIT);
+    }
+
+    private Splitter splitAndVerifyContent(byte[] b) throws InvalidKeySpecException, NoSuchAlgorithmException, EbicsException, IOException {
+        Splitter splitter = new Splitter(b);
+        splitter.readInput(true, new SecretKeySpec(secretKey().getEncoded(), "EAS"));
+        int segmentSize = splitter.getSegmentSize();
+        int segmentCount = splitter.getSegmentNumber();
+        assert (segmentSize < CHUNK_SIZE_LIMIT);
+        for (int i = 1; i <= segmentCount; i++) {
+            int contentLength = splitter.getContent(i).getContent().available();
+            assert (contentLength < CHUNK_SIZE_LIMIT);
+        }
+        return splitter;
+    }
+
+    private SecretKey secretKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        final String password = "test";
+        int pswdIterations = 65536;
+        int keySize = 256;
+        byte[] saltBytes = {0, 1, 2, 3, 4, 5, 6};
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec spec = new PBEKeySpec(
+                password.toCharArray(),
+                saltBytes,
+                pswdIterations,
+                keySize
+        );
+
+        return factory.generateSecret(spec);
+    }
+
+    private byte[] randomDataArray(int size) {
+        byte[] b = new byte[size];
+        new Random().nextBytes(b);
+        return b;
+    }
+}
