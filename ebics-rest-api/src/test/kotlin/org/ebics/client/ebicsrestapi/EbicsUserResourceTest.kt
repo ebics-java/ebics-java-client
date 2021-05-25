866838a62f357c84d136a40a02c82851b1bddc24
honza.toegel <honza.toegel@gmail.com>
2021-05-25 03:32:40 +0200

    Added kotlin no-args constructor support for JPA entity classes Fixed REST test

--- ebics-rest-api/src/test/kotlin/org/ebics/client/ebicsrestapi/EbicsUserResourceTest.kt
+++ ebics-rest-api/src/test/kotlin/org/ebics/client/ebicsrestapi/EbicsUserResourceTest.kt
@@ -1,9 +1,11 @@
 package org.ebics.client.ebicsrestapi
 
 import org.assertj.core.api.Assertions.assertThat
+import org.ebics.client.api.user.User
 import org.ebics.client.api.user.UserInfo
 import org.ebics.client.model.EbicsVersion
 import org.ebics.client.model.user.EbicsUserStatus
+import org.ebics.client.model.user.EbicsUserStatusEnum
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,10 +22,19 @@ class EbicsUserResourceTest (@Autowired private val restTemplate: TestRestTempla
     }
 
     @Test
-    fun addGetUser() {
+    fun addUserAndGet() {
         val user = UserInfo(null, EbicsVersion.H005, "CHT10001", "JT", "cn=JT,org=com", EbicsUserStatus(), null)
         val request: HttpEntity<UserInfo> = HttpEntity(user)
-        val userId = restTemplate.postForObject("/users", request, String::class.java)
-        assertThat(userId).isEqualTo("1")
+        val userId = restTemplate.postForObject("/users", request, Long::class.java)
+        assertThat(userId).isEqualTo(1)
+
+        val users = restTemplate.getForObject("/users", List::class.java)
+        assertThat(users.size).isEqualTo(1)
+        with (users[0] as UserInfo) {
+            assertThat(name).isEqualTo("JT")
+            assertThat(dn).isEqualTo("cn=JT,org=com")
+            assertThat(userStatus.status).isEqualTo(EbicsUserStatusEnum.CREATED)
+            assertThat(ebicsVersion).isEqualTo(EbicsVersion.H005)
+        }
     }
 }
