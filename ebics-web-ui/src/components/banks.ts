9f9fe322afd0ec571e13fa31cbbeb5c5a9d7f8fb
honza.toegel <honza.toegel@gmail.com>
2021-07-29 02:24:01 +0200

    Preparing web UI to move into ebic-java-client repository 2

---
+++ ebics-web-ui/src/components/banks.ts
@@ -0,0 +1,37 @@
+import { ref, onMounted } from 'vue';
+import { Bank } from 'components/models';
+import { useQuasar } from 'quasar';
+import { api } from 'boot/axios';
+
+/**
+ * Bank composition API in order to keep banks list updated from backend REST API
+ * @returns 
+ *  banks - reactive banks list
+ *  refreshBanksData - function to be called in order to refresh banks list from REST API
+ */
+export default function useBanksAPI() { 
+    const q = useQuasar();
+    const banks = ref<Bank[]>([]);
+
+    //Function to refresh banks data
+    const refreshBanksData = () => {
+        api
+          .get<Bank[]>('/banks')
+          .then((response) => {
+            banks.value = response.data;
+          })
+          .catch((error: Error) => {
+            q.notify({
+              color: 'negative',
+              position: 'bottom-right',
+              message: `Loading failed: ${error.message}`,
+              icon: 'report_problem',
+            });
+          });
+      }
+
+    //Banks data is refreshed by mounting
+    onMounted(refreshBanksData);
+
+    return {banks, refreshBanksData };
+}
---
+++ ebics-web-ui/src/router/routes.ts
@@ -0,0 +1,57 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Users.vue') },
+      { path: '/banks', component: () => import('pages/Banks.vue') },
+      {
+        path: '/bank',
+        name: 'bank/create',
+        component: () => import('pages/Bank.vue'),
+      },
+      {
+        path: '/bank/:id',
+        name: 'bank/edit',
+        component: () => import('pages/Bank.vue'),
+        props: (route) => {
+          const id = Number.parseInt(route.params.id as string, 10);
+          if (Number.isNaN(id)) {
+            return undefined;
+          }
+          return { id };
+        },
+      },
+      { path: '/users', component: () => import('pages/Users.vue') },
+      {
+        path: '/user',
+        name: 'user/create',
+        component: () => import('pages/User.vue'),
+      },
+      {
+        path: '/user/:id',
+        name: 'user/edit',
+        component: () => import('pages/User.vue'),
+        props: (route) => {
+          const id = Number.parseInt(route.params.id as string, 10);
+          if (Number.isNaN(id)) {
+            return undefined;
+          }
+          return { id };
+        },
+      },
+      { path: '/upload', component: () => import('pages/FileUpload.vue') },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue'),
+  },
+];
+
+export default routes;
