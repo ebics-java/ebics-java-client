8cd49aec5ff332a2f94ff12637382cdf85e4c297
honza.toegel <honza.toegel@gmail.com>
2021-08-08 03:49:27 +0200

    Added war starter

--- ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/EbicsRestApiApplication.kt
+++ ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/EbicsRestApiApplication.kt
@@ -2,6 +2,7 @@ package org.ebics.client.ebicsrestapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
@@ -11,7 +12,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @ComponentScan("org.ebics.client.api", "org.ebics.client.ebicsrestapi")
 @EntityScan("org.ebics.client.api")
 @EnableJpaRepositories("org.ebics.client.api")
-class EbicsRestApiApplication : SpringBootServletInitializer()
+class EbicsRestApiApplication : SpringBootServletInitializer() {
+
+	/**
+	 * Start app as war file
+	 */
+	override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+		return builder.sources(EbicsRestApiApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<EbicsRestApiApplication>(*args)
